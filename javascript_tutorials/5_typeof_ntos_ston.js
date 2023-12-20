b1c37136fe0be4b920a156921b50a45e4393eb31
Tarunkumar-33 <tarunkv169@gmail.com>
2023-12-20 18:15:48 +0530

    typeof num to string and string to num

---
+++ javascript_tutorials/5_typeof_ntos_ston.js
@@ -0,0 +1,25 @@
+
+//                    n to s ----------->  n + ""
+let age=22;
+age=age+"";
+console.log(typeof age,age);
+
+//                          or
+age=String(age);
+console.log(typeof age,age);
+
+
+
+
+
+//                    s to n ----------->  + "s"
+age="22";
+age=+age;
+console.log(typeof age,age);
+
+//                          or
+age=Number(age);
+console.log(typeof age,age);
+
+
+
