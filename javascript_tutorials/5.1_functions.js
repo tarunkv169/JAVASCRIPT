fb670b998e67370ee809b695be7da353fef2fed6
Tarunkumar-33 <tarunkv169@gmail.com>
2023-12-21 16:01:43 +0530

    functions

---
+++ javascript_tutorials/5.1_functions.js
@@ -0,0 +1,21 @@
+const hello=()=>{
+    console.log("hello is callled");
+    return "hello world";
+}
+
+let v=hello();
+console.log(v);
+
+const average=(a,b)=>{
+    console.log((a+b)/2);
+}
+
+average(4,5);
+
+//                   or
+
+function sum(a,b){
+    console.log(a+b);
+}
+
+sum(1,2);
