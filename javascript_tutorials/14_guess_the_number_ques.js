d59d5f10da4bed7d45518501e379cef8b85b089e
Tarunkumar-33 <tarunkv169@gmail.com>
2023-12-23 13:03:38 +0530

    exercise

---
+++ javascript_tutorials/14_guess_the_number_ques.js
@@ -0,0 +1,7 @@
+/* Write a javascript program to generate a random number and store it in a variavle. 
+the progrma then take an input from the user to tell then whether the guess was correct ,
+greater or lesser than the original number.
+  100 -(no. of guesses) is the score of the user. the program is expected to terminate 
+  once the number is guessed . the numver should be between 1-100 */
+
+  
