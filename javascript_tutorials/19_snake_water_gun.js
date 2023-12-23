1fbc6d5e45a90b6dd771d9cb86ff851595f4b157
Tarunkumar-33 <tarunkv169@gmail.com>
2023-12-23 13:09:34 +0530

    snake water gun

---
+++ javascript_tutorials/19_snake_water_gun.js
@@ -0,0 +1,1 @@
+// use javascript to create a game of snake water of gun. the game should ask you to enter s,w or g the computer shouk be able to randomly generate S,w or g and declare win or loss using alert . use confirm and promtp wherever rewquired
