f4f43dc257ecdf6606a72594dad39404e0456749
Tarunkumar-33 <tarunkv169@gmail.com>
2023-12-23 13:10:35 +0530

    children parent of ele dom

---
+++ javascript_tutorials/20_children_parent_of_ele_dom.js
@@ -0,0 +1,15 @@
+console.log("               about parent of element                 ");
+console.log(document.body.firstChild);
+console.log(document.body.lastchild);
+console.log(document.body.childNodes);
+// actual childnodes is not array we can make it 
+let child_arr = Array.from(document.body.childNodes);
+console.log(child_arr);
+console.log(child_arr[0]);
+console.log(child_arr[child_arr.length-1]);
+
+
+
+console.log("               about parent of element                 ");
+a=document.body.firstChild;
+console.log(a.parentNode);
