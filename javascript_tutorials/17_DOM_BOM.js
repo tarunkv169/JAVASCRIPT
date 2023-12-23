c59b86a75cb86e3e3269208bade3ea93c932bebc
Tarunkumar-33 <tarunkv169@gmail.com>
2023-12-23 13:05:43 +0530

    dom_bom

---
+++ javascript_tutorials/17_DOM_BOM.js
@@ -0,0 +1,6 @@
+//        DOM--dom(document object model) represents the page content as html
+document.body.style.background="green";
+
+//        BOM--bom(browser object model)  represents additional objects provided by the browser
+//                                        the function alert/cnfirm /promtp ar also part of bom 
+location.href = "https://google.com";
