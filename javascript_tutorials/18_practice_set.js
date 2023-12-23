64f9253332a801caeae6001a2975f96f1c008644
Tarunkumar-33 <tarunkv169@gmail.com>
2023-12-23 13:07:06 +0530

    practice set

---
+++ javascript_tutorials/18_practice_set.js
@@ -0,0 +1,33 @@
+// const compare_age=(age)=>
+// {
+//    return age<18?false:true;
+// }
+
+// let run_again=true;
+
+// while(run_again)
+// {
+//     age=+prompt("enter the age ");
+//     if(age<0)
+//     {
+//         console.error("please enter a valid age ");
+//         break;
+//     }
+//     else if(age=4)
+//     {
+//         location.href="https://google.com"
+//     }
+//     if(compare_age(age))
+//     {
+//         alert("you can drive ");
+//     }
+//     else{
+//         alert("you cannot drive ");
+//     }
+
+//     run_again=confirm("do u want to use prompt again ");
+// }
+
+
+let color=prompt("enter the color of page as b for blue etc ");
+document.body.style.background=color;
