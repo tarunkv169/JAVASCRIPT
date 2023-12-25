cce0db227c80cfa37391be7840b8d0b97ade789a
Tarunkumar-33 <tarunkv169@gmail.com>
2023-12-25 21:41:27 +0530

    table navigation

---
+++ javascript_tutorials/22_table_navigation.js
@@ -0,0 +1,9 @@
+let t = document.body.firstElementChild.firstElementChild;
+console.log(t);
+console.log(t.rows);
+console.log(t.caption);
+console.log(t.tHead);
+console.log(t.tfoot);
+console.log(t.rows[1].rowIndex);
+console.log(t.rows[2].cells);
+console.log(t.rows[1].cellIndex)
