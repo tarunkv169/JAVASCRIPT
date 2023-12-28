7e46233598b261d23dc7ad0e4f2fd56b737bcea0
Tarunkumar-33 <tarunkv169@gmail.com>
2023-12-28 13:07:35 +0530

    attributes methods js code

---
+++ javascript_tutorials/28_Attributes_methods.js
@@ -0,0 +1,22 @@
+// let first = document.getElementById("first");
+
+// // 1. hasAttribute ---------its it contain another attribute(like class) related to asked attribute
+// console.log(first.hasAttribute("class"));
+
+// // 2. getAttribute----------give name of asked attribute;
+// console.log(first.getAttribute("class"));
+
+// // 3. setAttribute----------change name of asked attribute;
+// first.setAttribute("class","guys");
+
+// // 4. removeAttribute-------remove the Attribute selected
+// first.removeAttribute("class");
+
+// // 5. .attributes-------give all the Attribute selected
+// console.log(first.attributes);
+
+
+// 6. data-X attributes
+console.log(first.dataset);
+console.log(first.dataset.game);
+console.log(first.dataset.player);
