71f50ec249dbd8b34306514727a8e4d6356c72ed
Tarunkumar-33 <tarunkv169@gmail.com>
2024-01-12 23:22:50 +0530

    practice set of arrays in js

--- javascript_tutorials/13_practice_set_arrays.js
+++ javascript_tutorials/13_practice_set_arrays.js
@@ -4,6 +4,8 @@
 // arr1.push(num1);
 // console.log(arr1);
 
+
+
 //keep adding numvers to the array in 1st until 0 is added to the array?
 // let num2;
 // let arr2=[1,2,3,4,5,6,7];
@@ -19,14 +21,27 @@
 // }
 // console.log(arr2);
 
+
+
 //fileter for numvers divisible by 10 from a given array
-let arr3=[9,10,11,20,15,30];
-let filter_arr=arr3.filter( (value)=>
-{
-    if(value%10==0)
-    {
-        return value;
-    }
+// let arr3=[9,10,11,20,15,30];
+// let filter_arr=arr3.filter( (value)=>
+// {
+//     if(value%10==0)
+//     {
+//         return value;
+//     }
+// })
+
+// console.log(filter_arr);
+
+
+
+//create an array of square of given numbers
+let arr4=[1,2,3,4,5,6]
+let arr5=arr4.map( (value)=>
+{    
+    return value*value;
 })
 
-console.log(filter_arr);
+console.log(arr5);
